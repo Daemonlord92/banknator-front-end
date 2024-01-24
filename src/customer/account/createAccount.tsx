4e073cff38c321e60ef61102d326f9a9b9171dbf
Matthew Jeshua Martin <matthew-martin@programmer.net>
2024-01-24 12:03:52 -0500

    createAccount component built

---
+++ src/customer/account/createAccount.tsx
@@ -0,0 +1,57 @@
+import {useState} from "react";
+import {Field, Form, Formik, FormikValues} from "formik";
+
+interface PostNewAccountInformation {
+    userProfileId:number,
+    accountType:string,
+    balance:number
+}
+
+export const CreateAccount = ({id, setDataChange}:{id:number, setDataChange: (arg0: boolean)=>void }) => {
+
+    const initialValues:PostNewAccountInformation = {userProfileId: id, accountType: "", balance:0.0}
+    const handleCreateAccount = async (data:PostNewAccountInformation, {resetForm}) => {
+        const response = await fetch("http://localhost:8080/apiv1/account/createNewAccount", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "http://localhost:5173",
+                "Authorization": "Bearer "+sessionStorage.getItem("Authorization")
+            },
+            body: JSON.stringify(data),
+        })
+        const result = await response.json();
+        window.alert(result.message)
+        resetForm()
+        setDataChange(true)
+    };
+    return (
+        <Formik initialValues={initialValues} onSubmit={handleCreateAccount}>
+            <Form className="container mx-auto" >
+                <h2 className="text-2xl font-bold mb-4">Create Account</h2>
+                <div className="mb-4">
+                    <label htmlFor="accountType" className="block text-sm font-medium text-gray-700">
+                        Account Type
+                    </label>
+                    <Field
+                        as="select"
+                        id="accountType"
+                        name="accountType"
+                        className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+                    >
+                        <option value="">Select Account Type</option>
+                        <option value="CHECKING">Checking</option>
+                        <option value="SAVING">Saving</option>
+                        <option value="CREDIT">Credit</option>
+                    </Field>
+                </div>
+                <button
+                    type="submit"
+                    className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
+                >
+                    Create Account
+                </button>
+            </Form>
+        </Formik>
+    );
+};
--- src/customer/customerDashboard.tsx
+++ src/customer/customerDashboard.tsx
@@ -6,29 +6,30 @@ import {useDispatch, useSelector} from "react-redux";
 import {getAllAccount, selectAccounts} from "../redux/slices/accountSlice.ts";
 import {getAllTransactionsByAccountNumber, selectTransactions} from "../redux/slices/transactionSlice.ts";
 import {TransactionList} from "./component/transactionList.tsx";
+import {CreateAccount} from "./account/createAccount.tsx";
 
 export const CustomerDashboard = () => {
-
     const decode = jwtDecode(sessionStorage.getItem("Authorization") || '')
     const dispatch = useDispatch()
-
     const accounts = useSelector(selectAccounts)
     const transactions = useSelector(selectTransactions)
+    const [dataChange, setDataChange] = useState(false)
+    async function getAllAccounts(id:number) {
+        const response = await fetch("http://localhost:8080/apiv1/account/getAccountInformationByUser?id="+id,{
+            method:"GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "http://localhost:5173",
+                "Authorization": "Bearer "+sessionStorage.getItem("Authorization")
+            }
+        })
+        const result = await response.json()
+        dispatch(getAllAccount(result))
+    }
     useEffect(() => {
-        async function getAllAccounts(id:number) {
-            const response = await fetch("http://localhost:8080/apiv1/account/getAccountInformationByUser?id="+id,{
-                method:"GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "http://localhost:5173",
-                    "Authorization": "Bearer "+sessionStorage.getItem("Authorization")
-                }
-            })
-            const result = await response.json()
-            dispatch(getAllAccount(result))
-        }
+        DataChange(false)
         getAllAccounts(decode.userProfileId)
-    }, [])
+    }, [dataChange]);
 
     const setTransactions= async (id:number) => {
         const response = await fetch("http://localhost:8080/apiv1/transaction/"+id,
@@ -44,7 +45,9 @@ export const CustomerDashboard = () => {
         console.table(result)
         dispatch(getAllTransactionsByAccountNumber(result))
     }
-
+    const DataChange=(hasDataChanged:boolean)=> {
+        setDataChange(hasDataChanged)
+    }
     return (
         <>
             <div className="space-y-10">
@@ -56,7 +59,7 @@ export const CustomerDashboard = () => {
                 </div>
                 <div className="flex align-text-top">
                     <h3 className="text-2xl font-bold text-start w-1/2">Accounts</h3>
-                    <Link to={"/createAccount"} className="text-end w-1/2">Create Account</Link>
+                    <div className="text-end w-1/2"><CreateAccount id={decode.userProfileId} setDataChange={DataChange}/></div>
                 </div>
                 <AccountCarousel accounts={accounts} setTransactions={setTransactions}/>
                 <div className="max-w-full flex align-text-bottom">
